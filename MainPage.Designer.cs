a8fbf15c9a2d771c712780c4c21154a2057e05e2
Sahil Bhandari <sahilshailu7977460287@gmail.com>
2023-02-19 10:52:59 +0530

    Initialized MainPage form

--- MainPage.Designer.cs
+++ MainPage.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1087, 750);
+            this.Name = "MainPage";
+            this.Text = "Minecraft Bedrock Edition Launcher";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
