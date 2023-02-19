e221de55c48972c52c8f62d68cd2e001d84286b6
Sahil Bhandari <sahilshailu7977460287@gmail.com>
2023-02-19 12:25:44 +0530

    Added more views and a helper class

---
+++ Views/HomeView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Launch_Script.Views
+{
+    public partial class HomeView : Form
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Views/NewMethodBypassView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Launch_Script.Views
+{
+    public partial class NewMethodBypassView : Form
+    {
+        public NewMethodBypassView()
+        {
+            InitializeComponent();
+        }
+
+        private void NewMethodBypassView_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
